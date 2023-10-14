0611aaa556b71ab58e95dbb41678dd458538243d
Max <maxwangko1@gmail.com>
2023-10-13 21:47:11 -0400

    add homepage

---
+++ MorseCode/MorseCode/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  MorseCode
+//
+//  Created by Max Ko on 10/13/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
