bcad75942f863a5512780590a96f3d03187baa55
Max <maxwangko1@gmail.com>
2023-10-13 21:43:04 -0400

    add xcode proj

---
+++ MorseCode/MorseCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MorseCode
+//
+//  Created by Max Ko on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MorseCode/MorseCode/MorseCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseCodeApp.swift
+//  MorseCode
+//
+//  Created by Max Ko on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MorseCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
