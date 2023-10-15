6ad8ab4b34ce32d52392d0636a9018951658b81a
Max <maxwangko1@gmail.com>
2023-10-14 22:51:28 -0400

    add video to frame func

--- MorseCode/MorseCode/VideoView.swift
+++ MorseCode/MorseCode/VideoView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import UIKit
 import CoreGraphics
+import AVFoundation
 
 struct VideoView: View {
 
@@ -26,18 +27,31 @@ struct VideoView: View {
     
     func helperLoadImgArray() {
         
-        if let grayscalePixels = grayscalePixelArray(fromImageNamed: "tiny") {
-            for row in grayscalePixels {
-                print(row)
+        guard let videoURL = Bundle.main.url(forResource: "IMG_7605", withExtension: "mov")
+        else {
+            print("Video file not found.")
+            return
+        }
+        
+        let frames = extract2(from: videoURL)
+
+        for (iframe, uiimg) in frames.enumerated() {
+        
+            if let grayscalePixels = grayscalePixelArray(fromImageNamed: uiimg) {
+                print("frame \(iframe)")
+                for (irow, row) in grayscalePixels.enumerated() {
+                    print("row \(irow)")
+                    print(row)
+                }
+            } else {
+                print("Error loading image or extracting pixel data.")
             }
-        } else {
-            print("Error loading image or extracting pixel data.")
         }
         
     }
     
-    func grayscalePixelArray(fromImageNamed imageName: String) -> [[UInt8]]? {
-        guard let image = UIImage(named: imageName) else {
+    func grayscalePixelArray(fromImageNamed imageName: UIImage?) -> [[UInt8]]? {
+        guard let image = imageName else {
             // Handle image loading error
             print("UI Image error")
             return nil
@@ -75,7 +89,11 @@ struct VideoView: View {
                 if let pixelData = context.data {
                     let offset = 1 * (width * y + x)
                     let pixelValue = pixelData.load(fromByteOffset: offset, as: UInt8.self)
-                    row.append(pixelValue)
+                    if pixelValue > 200 {
+                        row.append(1)
+                    } else {
+                        row.append(0)
+                    }
                 }
             }
             grayscalePixels.append(row)
@@ -83,6 +101,34 @@ struct VideoView: View {
         return grayscalePixels
     }
     
+    func extract2(from videoURL: URL) -> [UIImage] {
+        var frames: [UIImage] = []
+
+        let asset = AVAsset(url: videoURL)
+        let assetDuration = CMTimeGetSeconds(asset.duration)
+        let frameRate = 5.0 // set number of fps
+
+        let generator = AVAssetImageGenerator(asset: asset)
+        generator.requestedTimeToleranceBefore = CMTime.zero
+        generator.requestedTimeToleranceAfter = CMTime.zero
+
+        let frameDuration = CMTimeMake(value: 1, timescale: Int32(frameRate))
+        var currentTime = CMTime.zero
+
+        while CMTimeCompare(currentTime, asset.duration) == -1 {
+            do {
+                let cgImage = try generator.copyCGImage(at: currentTime, actualTime: nil)
+                let uiImage = UIImage(cgImage: cgImage)
+                frames.append(uiImage)
+            } catch {
+                print("Error extracting frame at time \(CMTimeGetSeconds(currentTime)): \(error)")
+            }
+
+            currentTime = CMTimeAdd(currentTime, frameDuration)
+        }
+
+        return frames
+    }
 }
 
 #Preview {
